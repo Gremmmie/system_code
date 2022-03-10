faadb8fc7fcdea4e3f7c7d9ed0471e304bb657d0
Gremmie <2455380921@qq.com>
2022-03-10 06:05:08 +0000

    add LICENSE.

---
+++ src/hhh.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description: Hello,I would appreciate your comments~
+ * User: Gremmie
+ * Date: 2022-03-10
+ * Destination:
+ */
+public class hhh {
+    public static void main(String[] args) {
+        System.out.println("hhh");
+    }
+}
