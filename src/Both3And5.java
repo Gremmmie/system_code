0a55968d145317cd5350bf9efae7d855e06fed71
Gremmie <2455380921@qq.com>
2022-03-11 20:01:30 +0800

    Merge remote-tracking branch 'origin/master'

--- src/Both3And5.java
+++ src/Both3And5.java
@@ -7,6 +7,20 @@
  */
 public class Both3And5 {
     public static void main(String[] args) {
+        int sum1 = 0,sum2=0;
+        int i,j;
+        for(i=1;i<=100;i++){
+            if(i%2==1){
+                sum1 += i;
+            }else {
+                sum2 += i;
+            }
+        }
+        System.out.println(sum1+" "+sum2);
+    }
+
+
+    public static void main1(String[] args) {
         int i=1;
         while(i<=100){
             if(i%15==0){
