b929b5a05cbd0e24f7d3f235d055abdb93a72e52
Gremmie <2455380921@qq.com>
2022-03-20 14:45:57 +0800

    Merge remote-tracking branch 'origin/master'

--- 2022_3_20/src/arrDemo.java
+++ 2022_3_20/src/arrDemo.java
@@ -8,13 +8,32 @@
 public class arrDemo {
 
     public static void main(String[] args) {
-        int[] array;
+        int[] array = {1,2,3};
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]+" ");
+        }
+
+
+
+//        第二种打印方式为for-each  增强for循环
+        for( int x   :  array  ){
+            System.out.println(x+" ");
+        }
+//        这么做会每次循环把array中的数据赋值给x
+    }
+
+
+    public static void main3(String[] args) {
+        int[] array ;
+//      我们称这样为动态初始化
         array = new int[]{1,2,3,4,5};
         array = new int[]{9,8,7,6,5};
 //      这样重新定义是可以的
 
         int[] array2 = {1,2,3,4,5};
-        //array2 = {9,8,7,6,5}这样写会报错，因为定义数组的时候整体赋值，只有一次机会
+//      我们称这么做为静态初始化
+//
+//      array2 = {9,8,7,6,5}这样写会报错，因为定义数组的时候整体赋值，只有一次机会
     }
 
     public static void main2(String[] args) {
