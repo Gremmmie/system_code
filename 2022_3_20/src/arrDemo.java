4053c720fd5bb019e66e345d5223d75993e031e7
Gremmie <2455380921@qq.com>
2022-03-20 11:35:02 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ 2022_3_20/src/arrDemo.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description: Hello,I would appreciate your comments~
+ * User:
+ * Date: -03-20
+ * Destination:
+ */
+public class arrDemo {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        int[] array2 = new int[]{1,2,3,4,5,6,7,8,9,10};
+//      两种是一样的,一般用第一种
+        int[] array3 = new int[10];
+//      不确定里面放什么的话，就用第三种
+    }
+}
