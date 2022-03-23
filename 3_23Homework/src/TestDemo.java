67e17121b29c4d3e047f50688c39d8db74eba149
Gremmie <2455380921@qq.com>
2022-03-23 18:14:18 +0800

    Merge remote-tracking branch 'origin/master'

--- 3_23Homework/src/TestDemo.java
+++ 3_23Homework/src/TestDemo.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created with IntelliJ IDEA.
  * Description: Hello,I would appreciate your comments~
@@ -7,8 +9,20 @@
  */
 public class TestDemo {
     public static void main(String[] args) {
-
+        ArrayList<Integer> a4[]=new ArrayList[3];
+        for(int i=0;i<a4.length;i++) {
+            a4[i]=new ArrayList();
+            a4[i].add(1);
+            for(int j:a4[i])
+                System.out.print(j+" ");
+            System.out.println();
+        }
     }
-    public static int BinarySearch
+    public static int BinarySearch(int[] array,int key){
+        int index,left,right;
+        left = 0;
+        right = array.length-1;
+        return 1;
+    }
 
 }
