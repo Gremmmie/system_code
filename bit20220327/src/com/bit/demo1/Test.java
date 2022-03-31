cc1e61bd64ba2e84de38ac510e93cca8a8013439
Gremmie <2455380921@qq.com>
2022-03-31 15:07:12 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ bit20220327/src/com/bit/demo1/Test.java
@@ -0,0 +1,17 @@
+package com.bit.demo1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: Hello,I would appreciate your comments~
+ * User:
+ * Date: -03-31
+ * Destination:
+ */
+public class Test {
+    public static void main(String[] args) {
+        TestDemo testDemo = new TestDemo();
+        System.out.println(testDemo.a);
+        System.out.println(testDemo.b);
+        System.out.println(testDemo.c);
+    }
+}
---
+++ bit20220327/src/com/bit/demo2/TestDemo2.java
@@ -0,0 +1,22 @@
+package com.bit.demo2;
+
+import com.bit.demo1.TestDemo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: Hello,I would appreciate your comments~
+ * User:
+ * Date: -03-31
+ * Destination:
+ */
+public class TestDemo2 {
+
+
+    public static void main(String[] args) {
+        TestDemo testDemo = new TestDemo();
+        System.out.println(testDemo.a);
+        System.out.println(testDemo.b);
+        System.out.println(testDemo.c);
+
+    }
+}
