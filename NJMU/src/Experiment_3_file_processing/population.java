0bbfbbe3c6b9ce2e8456650fee40de9637a7210c
Gremmie <2455380921@qq.com>
2022-04-14 21:56:15 +0800

    ceshi

---
+++ NJMU/src/Experiment_3_file_processing/population.java
@@ -0,0 +1,55 @@
+package Experiment_3_file_processing;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: Hello,I would appreciate your comments~
+ * User:
+ * Date: -04-14
+ * Destination:
+ */
+
+import java.io.*;
+import java.util.*;
+public class population {
+
+    public static void main(String[] args)throws FileNotFoundException {
+        Scanner console=new Scanner(System.in);
+        System.out.println("name?");
+//      先用户输入姓名
+        Scanner babyName=new Scanner(new File("E:\\Text.txt"));
+//      从文件中读取姓名
+        judge(console,babyName);
+//      判别文件中第一行的姓名与用户输入的姓名是否一致
+        babyName.close();
+//      清空scanner流
+
+    }
+
+    public static void judge(Scanner console,Scanner babyName) {
+        String names=console.nextLine();
+//        用name接受console的流，console置为空
+        while(babyName.hasNextLine()) {
+            String data=babyName.nextLine();
+            Scanner again=new Scanner(data);
+            if(again.next().equalsIgnoreCase(names)) {
+                while(again.hasNextInt()) {
+                    int num=again.nextInt();
+                    System.out.println(num);
+
+                }
+            }
+
+        }
+//        String names1=console.nextLine();
+//        while(babyName.hasNextLine()) {
+//            String data=babyName.nextLine();
+//            Scanner again=new Scanner(data);
+//            if(again.next().equalsIgnoreCase(names1)) {
+//                System.out.println("*");
+//
+//            }
+//        }
+
+    }
+
+}
