02d22d528f49cb8ac1eab0186e9d79e6d9269c96
Gremmie <2455380921@qq.com>
2022-07-21 21:41:28 +0800

    多线程

--- system_code/src/thread/Demo6.java
+++ system_code/src/thread/Demo6.java
@@ -27,7 +27,40 @@ public class Demo6 {
         System.out.println("单线程消耗时间:"+(end-beg)+"ms");
     }
 
+    private static void concurrency(){//并发
+        long beg = System.currentTimeMillis();
+        Thread t1 = new Thread(()->{
+            int a = 0;
+            for (long i =0;i<COUNT;i++){
+                a++;
+            }
+        });
+        Thread t2 = new Thread(()->{
+            int a = 0;
+            for (long i =0;i<COUNT;i++){
+                a++;
+            }
+        });
+        t1.start();
+        t2.start();
+
+        try{
+            t1.join();//这里的join是等待线程结束,等待线程把自己的run方法执行完
+            //在主线程中调用t1.join,意思就是让main线程等t1执行完
+            t2.join();
+            //t1 t2是会开始执行,同时不等t1 t2执行玩,main线程就往下走了,于是就结束计时
+            //此处的即使,是为了衡量t1和t2的执行时间,正确的做法应该是等到t1和t2都执行完再计时
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        long end = System.currentTimeMillis();
+        System.out.println("单线程消耗时间:"+(end-beg)+"ms");
+    }
+
     public static void main(String[] args) {
-        serial();
+//        serial();
+        concurrency();
     }
 }
