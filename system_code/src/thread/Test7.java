58d7aced0583a3e67efa9e14db601f5020db0616
Gremmie <2455380921@qq.com>
2022-07-31 14:21:37 +0800

    多线程

--- system_code/src/thread/Test7.java
+++ system_code/src/thread/Test7.java
@@ -1,7 +1,31 @@
 package thread;
 
 public class Test7 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        Runnable target = () -> {
+          for (int i=0;i<3;i++){
+              System.out.println(Thread.currentThread().getName()
+              +":我在工作中~");
+              try {
+                  Thread.sleep(1000);
+              } catch (InterruptedException e) {
+                  e.printStackTrace();
+              }
+          }
+            System.out.println(Thread.currentThread().getName()
+            +":我运行结束了");
+        };
+        Thread thread1 = new Thread(target,"何二傻");
+        Thread thread2 = new Thread(target,"葛玉米");
+        System.out.println(Thread.currentThread().getName()+
+                ":何二傻先开始工作");
+        thread1.start();
+        thread1.join();
+        System.out.println(Thread.currentThread().getName()
+                +":何二傻工作结束了,换葛玉米上来干活");
+        thread2.start();
+        thread2.join();
+        System.out.println(Thread.currentThread().getName()
+                +":葛玉米工作结束");
     }
 }
